2fd1d200850905da3dc416ade13b313a7f03855b
notsheepdog <notsheepdog@gmail.com>
2022-03-07 18:32:27 -0500

    1st level done (aside from end dialogue)

--- Gacha Bullet Hell/Assets/Scripts/Scriptable Objects/BossProfile.cs
+++ Gacha Bullet Hell/Assets/Scripts/Scriptable Objects/BossProfile.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Boss", menuName = "Bosses")]
 public class BossProfile : ScriptableObject
 {
-    [Header("With (CHAR 1)")]
+    [Header("With Sirena")]
     public DialogueLine[] C1preBattleConversation;
     public DialogueLine[] C1easyPostBattleConversation;
     public DialogueLine[] C1hardPostBattleConversation;
 
-    [Header("With (CHAR 2)")]
+    [Header("With Dot")]
     public DialogueLine[] C2preBattleConversation;
     public DialogueLine[] C2easyPostBattleConversation;
     public DialogueLine[] C2hardPostBattleConversation;
 
-    [Header("With (CHAR 3)")]
+    [Header("With Renji")]
     public DialogueLine[] C3preBattleConversation;
     public DialogueLine[] C3easyPostBattleConversation;
     public DialogueLine[] C3hardPostBattleConversation;
