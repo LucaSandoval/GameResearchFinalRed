3807114ed269ecfc9c9f047f08a376db8f424435
Luca Sandoval <32229145+LucaSandoval@users.noreply.github.com>
2022-04-13 19:51:30 -0400

    Make score and system for getting lives

--- Gacha Bullet Hell/Assets/ScoreManager.cs
+++ Gacha Bullet Hell/Assets/ScoreManager.cs
@@ -9,10 +9,46 @@ public class ScoreManager : MonoBehaviour
     public static float score;
     public static float scoreToAdd;
 
+    private PlayerStatController stat;
+
+    public static lifeBonus[] lifeBonuses = new lifeBonus[6] 
+    { new lifeBonus(1000, false), new lifeBonus(3000, false), new lifeBonus(5000, false),
+    new lifeBonus(7000, false), new lifeBonus(9000, false), new lifeBonus(11000, false)};
+
+    public void Start()
+    {
+        stat = GameObject.Find("Player").GetComponent<PlayerStatController>();
+    }
+
+    [System.Serializable]
+    public struct lifeBonus
+    {
+
+        public float pointValue;
+        public bool unlocked;
+
+        public lifeBonus(float point, bool unlocked)
+        {
+            this.pointValue = point;
+            this.unlocked = unlocked;
+        } 
+    }
+
     // Update is called once per frame
     void Update()
     {
-        scoreText.text = score.ToString("00000000"); 
+        scoreText.text = score.ToString("000000"); 
+
+        for (int i = 0; i < lifeBonuses.Length; i++)
+        {
+            if (lifeBonuses[i].unlocked == false && score >= lifeBonuses[i].pointValue)
+            {
+                PlayerStatController.lives += 1;
+                stat.GenerateLivesIcons();
+                lifeBonuses[i].unlocked = true;
+                Debug.Log("life added");
+            }
+        }
     }
 
     private void FixedUpdate()
